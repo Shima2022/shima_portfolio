caa9f520ad6a8a06dccc71b2d006d114dc86354a
Shima2022 <shima.heidari@outlook.com>
2022-12-14 10:20:27 +0200

    bubble animation is done

--- src/components/Cv/cv.tsx
+++ src/components/Cv/cv.tsx
@@ -7,78 +7,79 @@ type CvProps = {
 
 const Cv: React.FC<CvProps> = () => {
   return (
-
     <><div>
       {/* <div className={styles.star}></div>
-        <link rel="stylesheet" type="text/css" href="style.css" />
-                <section>
-                  <span></span>
-                  <span></span>
-                  <span></span>
-                  <span></span>
-                  <span></span>
-                  <span></span>
-                  <span></span>
-                  <span></span>
-                  <span></span>
-                  <span></span>
-                </section> */}
+    <link rel="stylesheet" type="text/css" href="style.css" />
+            <section>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+            </section> */}
+    </div>
+    
+    <div>
+        <div className={styles.bubble}>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+    </div>
+        <div className={styles.bubble}>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+    </div>
+        <div className={styles.bubble}>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+    </div>
+        <div className={styles.bubble}>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+    </div>
+        <div className={styles.bubble}>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
     </div>
 
-      {/* <div className={styles.bubble}></div> */}
-      <div className={styles.bubble}>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-      <div className={styles.bubble}>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-      <div className={styles.bubble}>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-      <div className={styles.bubble}>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-      <div className={styles.bubble}>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
+        <div className={styles.container}>
+          <h1>
+            CURRICULUM VITAE
+          </h1>
+          <div className={styles.cvSection}></div>
+          <h2>EDUCATION</h2>
+          <p>Vantaa Vocational College, Varia<br></br>
+            Sofware developer</p>
 
-      <div className={styles.container}>
-        <h1>
-          CURRICULUM VITAE
-        </h1>
-        <div className={styles.cvSection}></div>
-        <h2>EDUCATION</h2>
-        <p>Vantaa Vocational College, Varia<br></br>
-          Sofware developer</p>
-
-        <br></br>
-        <div className={styles.cvSection}>
-          <h3>Work</h3>
-          <p>Unity games <br></br>
-            Varia project</p>
+          <br></br>
+          <div className={styles.cvSection}>
+            <h3>Work</h3>
+            <p>Unity games <br></br>
+              Varia project</p>
+          </div>
         </div>
-      </div></>
+      </div>
+      </>
   )
 }
 
-export default Cv
+        export default Cv
